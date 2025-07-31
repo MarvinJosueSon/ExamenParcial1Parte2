cc74f62e79920798fb894c87a56752c7faf3091a
Marvin <sonmarvint2001@gmail.com>
2025-07-30 18:22:38 -0600

    Opcion 3 y opcion 5

---
+++ main.py
@@ -0,0 +1,19 @@
+def contarLetras(cadena,letra):
+    if cadena=="":
+        return 0
+    if letra==cadena[0]:
+        return 1 + contarLetras(cadena[1:],letra)
+    else:
+        return contarLetras(cadena[1:],letra)
+print(contarLetras("abracadabra","a"))
+
+def contarDigitos(numero):
+    if numero==0:
+        return 0
+    elif numero<10:
+        return 1 + contarDigitos(numero//10)
+    else:
+        return 1 + contarDigitos(numero//10)
+
+print(contarDigitos(1234567))
+
